7e4b17df445668a23afc6c0d608dc0b81b06c975
Salvatore D'Amore <salvatore.damore01@yahoo.com>
2023-01-05 18:04:32 +0100

    index.js
    create connection with mongodb
    And Run LocalHost

---
+++ index.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://salvo:Salvodamore2001@newcluster.uj0lev5.mongodb.net/CookBook");
+const path = require("path");
+
+const express = require("express");
+const app = express();
+
+//for user routes
+const userRoute = require('./routes/userRoute');
+app.use('/',userRoute);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000,function(){
+    console.log("Server is running");
+})

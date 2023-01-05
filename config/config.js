bea91c40040a92e446c39bf5438de2b26fada2ac
Salvatore D'Amore <salvatore.damore01@yahoo.com>
2023-01-05 18:11:32 +0100

    create file config.js
    the credentials for send mails

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const sessionSecret = "mysitesessionsecret";
+
+const emailUser = "salvodamore2301@gmail.com";
+const emailPassword = "tsej hbza pdil spfs";
+
+module.exports ={
+    sessionSecret,
+    emailUser,
+    emailPassword
+}

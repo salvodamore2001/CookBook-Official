f6ffcb099f6bcbbc28ae4331431fcfef88da7d91
Salvatore D'Amore <salvatore.damore01@yahoo.com>
2023-01-06 13:48:39 +0100

    Recipes Blog

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,10 +1,21 @@
+const { now } = require('mongoose');
 const Recipe = require('../models/recipeModels');
 
 const loadRecipe = async(req,res)=>{
 
     try {
         
-        res.render('home');
+        const recipes = [{
+            title: 'Recipe Test Title',
+            createdAt: new Date(),
+            description: 'Recipe Test Description'
+        },
+    {
+        title: 'Recipe Test2 Title',
+        createdAt: new Date(),
+        description: 'Recipe Test2 Description'
+        }]
+        res.render('home', {recipes: recipes});
 
     } catch (error) {
         console.log(error.message);

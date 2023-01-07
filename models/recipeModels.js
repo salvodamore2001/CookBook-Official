15edcbb9581b220e9668802e71c1894aad0e1477
Salvatore D'Amore <salvatore.damore01@yahoo.com>
2023-01-07 18:04:42 +0100

    install some libraries + recipeSchema

--- models/recipeModels.js
+++ models/recipeModels.js
@@ -1,4 +1,9 @@
 const mongoose = require("mongoose");
+const marked = require('marked');
+const slugify = require('slugify');
+const createDomPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const dompurify = createDomPurify(new JSDOM().window);
 
 const recipeSchema = new mongoose.Schema({
 
@@ -21,7 +26,28 @@ const recipeSchema = new mongoose.Schema({
     createAt:{
         type: Date,
         default: Date.now
-    }
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+      },
+      sanitizedHtml: {
+        type: String,
+        required: true
+      }
 });
+
+recipeSchema.pre('validate', function(next) {
+    if (this.title) {
+      this.slug = slugify(this.title, { lower: true, strict: true })
+    }
+  
+    if (this.markdown) {
+      this.sanitizedHtml = dompurify.sanitize(marked(this.ingredients))
+    }
+  
+    next()
+  })
 
 module.exports = mongoose.model('Recipe',recipeSchema);

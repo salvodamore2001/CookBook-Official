be3f7ad1459584bb2946fea6f3ed24b0d8a7a9e2
Salvatore D'Amore <salvatore.damore01@yahoo.com>
2023-01-07 18:40:01 +0100

    some changes

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,28 +1,6 @@
 const { now } = require('mongoose');
 const Recipe = require('../models/recipeModels');
 
-const loadRecipe = async(req,res)=>{
-
-    try {
-        
-        const recipes = [{
-            title: 'Recipe Test Title',
-            createdAt: new Date(),
-            description: 'Recipe Test Description'
-        },
-    {
-        title: 'Recipe Test2 Title',
-        createdAt: new Date(),
-        description: 'Recipe Test2 Description'
-        }]
-        res.render('home', {recipes: recipes});
-
-    } catch (error) {
-        console.log(error.message);
-    }
-
-}
-
 const newRecipe = async(req,res)=>{
 
 try {
@@ -35,22 +13,6 @@ try {
 
 }
 
-const insertRecipe = async(req,res)=>{
-
-try {
-    
-    const recipe = new Recipe({
-
-    });
-
-} catch (error) {
-    console.log(error.message);
-}
-
-}
-
 module.exports = {
-    loadRecipe,
-    newRecipe,
-    insertRecipe
+    newRecipe
 }
--- models/recipeModels.js
+++ models/recipeModels.js
@@ -1,9 +1,4 @@
 const mongoose = require("mongoose");
-const marked = require('marked');
-const slugify = require('slugify');
-const createDomPurify = require('dompurify');
-const { JSDOM } = require('jsdom');
-const dompurify = createDomPurify(new JSDOM().window);
 
 const recipeSchema = new mongoose.Schema({
 
@@ -22,32 +17,7 @@ const recipeSchema = new mongoose.Schema({
     image:{
         type:String,
         required:true
-    },
-    createAt:{
-        type: Date,
-        default: Date.now
-    },
-    slug: {
-        type: String,
-        required: true,
-        unique: true
-      },
-      sanitizedHtml: {
-        type: String,
-        required: true
-      }
+    }
 });
-
-recipeSchema.pre('validate', function(next) {
-    if (this.title) {
-      this.slug = slugify(this.title, { lower: true, strict: true })
-    }
-  
-    if (this.markdown) {
-      this.sanitizedHtml = dompurify.sanitize(marked(this.ingredients))
-    }
-  
-    next()
-  })
 
 module.exports = mongoose.model('Recipe',recipeSchema);
